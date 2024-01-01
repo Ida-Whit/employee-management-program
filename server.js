adaa8f20464cda46d138b26f206fa6dcee591541
Ida Whitcomb <idawhitcomb@gmail.com>
2024-01-01 12:30:21 -0500

    My if statements in server.js did not seem to be working, switched that over to a switch statement and now is working perfectly.

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_management_db database.`)
 );
 
-inquirer
+//Inquirer function to allow user to interact with management system.
+function init() {
+  inquirer
   .prompt([
     {
       type: 'list',
@@ -34,75 +36,107 @@ inquirer
       'Exit.']
     }
   ]) .then((response) => {
-    if(response.answer === 'View all departments.') {
-      viewDepartment();
-    } else if (response.answer === 'View all roles.') {
-      viewRoles();
-    } else if (response.answer === 'View all employees.') {
-      viewEmployees();
-    } else if (response.answer === 'Add a department.') {
-      addDepartment();
-    } else if (response.answer === 'Add a role.') {
-      addRole();
-    } else if (response.answer === 'Add an employee.') {
-      addEmployee();
-    } else if (response.answer === 'Update an employee role.') {
-      changeRole();
-    } else {
-      db.end();
-        console.log("Thanks for using Employee Management System.")
-    }
-  })
+    switch(response.answer) {
+      case 'View all departments.':
+        viewDepartment();
+        break;
+      case 'View all roles.':
+        viewRoles();
+        break;
+      case 'View all employees.':
+        viewEmployees();
+        break;
+      case 'Add a department.':
+        addDepartment();
+        break;
+      case 'Add a role.':
+        addRole();
+        break;
+      case 'Add an employee.':
+        addEmployee();
+        break;
+      case 'Update an employee role.':
+        changeRole();
+        break;
+      case 'Exit.':
+        db.end();
+          console.log('Connection closed. Thank you for using the employe management system.');
+          break;
+    };
+  });
+};
 
-
+//List out all functions needed to carry out management system options.
 
 //View all departments
 function viewDepartment(){
-  db.query('SELECT * FROM department'), function (err, results) {
+  db.query('SELECT * FROM department', function (err, results) {
     console.log(results);
-  };
-  //Start inquirer function again
+    init();
+  });
 }
 //View all roles
+//job title, role id, department for that role and salary for that role
 function viewRoles(){
-  db.query('SELECT * FROM role'), function (err, results) {
+  db.query('SELECT * FROM role', function (err, results) {
     console.log(results);
-  };
-  //Start inquirer function again
+    init();
+  });
 }
 //View all employees
+//employee first and last name, job title, department, salary, manager
 function viewEmployees(){
-  db.query('SELECT * FROM employee'), function (err, results) {
+  db.query('SELECT * FROM employee', function (err, results) {
     console.log(results);
-  };
-  //Start inquirer function again
+    init();
+  });
 }
 //Add a department
+//prompted to enter name of department and it gets added
 function addDepartment(){
-
-  //Start inquirer function again
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Please enter new department name.",
+      name: "department"
+    },
+  ]) .then((response) => {
+    db.query('INSERT INTO department (department_name) VALUE(?)', function (err, results) {
+      console.log(results);
+    });
+    db.query('SELECT * FROM department', function (err, results) {
+      console.log(results);
+      init();
+    });
+  });
 }
 //Add a role
+//prompted to enter name, salary, department for the role and role gets added
 function addRole(){
 
-  //Start inquirer function again
+  init();
 }
 //Add an employee
+//first and last name, role, manager and employee gets added
 function addEmployee(){
 
-  //Start inquirer function again
+  init();
 }
 //Update an employees role
+//prompted to select an employee and update their role and the database is updated.
 function changeRole(){
 
-  //Start inquirer function again
+  init();
 }
+
+init();
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
+//Let user know the program is live.
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  }); 
+  });
